82d70fa6154daac8a0da2f107a4b23a19270db44
Avery <averylisneil@gmail.com>
2019-10-31 19:28:52 -0700

    Ripped out backend.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path')
-const controller = require('../database/dbMethods.js');
 const cors = require('cors');
 const app = express()
 const port = 9004
@@ -18,28 +17,5 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
 
-app.get("/allHomes", (req,res) => {
-  // console.log('getting get req inside server function');   //get by specific id
-  controller.getAllFromHomes((err,dbObj)=> {
-    if (err) {
-      console.log('error from index.js SERVER******')
-      res.status(500).send(err);
-    } else {
-      console.log('SENT All')
-      res.status(200).send(dbObj);
-    }
-  })
-}) 
 
-
-// app.get("/currListing", (req,res) => {
-//   controller.getCurrListing((err,dbObj)=> {
-//     if (err) {
-//       res.status(500).send(err);
-//     } else {
-//       console.log('SENT current listing')
-//       res.status(200).send(dbObj);
-//     }
-//   })
-// })
 
